196d0875c3be831eeebfc574be795b7fcdf37b32
Sasongko Bawono <sasongko262@gmail.com>
2017-08-14 11:48:15 +0700

    modified doinst.sh

--- doinst.sh
+++ doinst.sh
@@ -1,5 +1,4 @@
 rm -rf /usr/share/doc/KBBI-Qt
-mv /usr/share/applications/KBBI-Qt.desktop /usr/share/applications/kbbi-qt.desktop
 ln -sf /usr/bin/KBBI-Qt /usr/bin/kbbi-qt
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
